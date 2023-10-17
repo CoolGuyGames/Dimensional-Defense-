c1444285e74cd415c9e1a3c101e19a970ed50f90
CoolGuyGames <theforestgriefer@gmail.com>
2023-10-17 09:19:20 -0400

    Added lots of stuff

--- Tower Defense Game/Assets/Level 1/Scripts/ProjectileScript.cs
+++ Tower Defense Game/Assets/Level 1/Scripts/ProjectileScript.cs
@@ -7,14 +7,27 @@ public class ProjectileScript : MonoBehaviour
     public int damage;
     public string dimension;
 
+    private ParticleSystem explosionParticles;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Enemy"))
+        explosionParticles = this.GetComponentInChildren<ParticleSystem>();
+
+        StartCoroutine(Explode(collision));
+    }
+
+    IEnumerator Explode(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
         {
-            if(dimension == collision.GetComponent<EnemyMovement>().dimension)
-                collision.GetComponent<EnemyMovement>().health -= damage * 2;    
+            this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            this.transform.Find("Trail").gameObject.SetActive(false);
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            if (dimension == collision.GetComponent<EnemyMovement>().dimension)
+                collision.GetComponent<EnemyMovement>().health -= damage * 2;
             else
                 collision.GetComponent<EnemyMovement>().health -= damage;
+            explosionParticles.Play();
+            yield return new WaitForSeconds(explosionParticles.main.duration);
             Destroy(gameObject);
         }
     }
